72a71cd1a2a6ca8444ca820493a2a2caac478c32
Ian Effendi <effendiian@gmail.com>
2019-05-01 05:40:08 -0400

    Create model.

---
+++ weight_a_minute/model.py
@@ -0,0 +1,29 @@
+"""
+Represents the game model and keeps track of state.
+"""
+
+from suitcases import fraction as f
+from suitcases import generate_fractions as gfrac
+import random as r
+
+class WAMGameModel:
+    def __init__(self, previousModel=None):
+        r.seed()
+        if previousModel is not None:
+            self.base_frac = previousModel.base_frac
+            self.num_over = previousModel.num_over
+            self.frac_list = previousModel.frac_list
+
+    def start_game(self):
+        self.base_frac = gfrac.generate_base_frac(10, 4)
+        self.num_over = r.randint(2, 5)
+        self.frac_list = gfrac.generate_fracs(self.base_frac, 7 - self.num_over, self.num_over, 1, 16, 30)
+
+    def check_bag(self, index):
+        if self.base_frac.compare(self.frac_list[index]) == -1:
+            self.frac_list.pop(index)
+            self.num_over -= 1
+            if self.num_over == 0:
+                return 1
+            return 0
+        return -1
